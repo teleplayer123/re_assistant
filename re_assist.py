9b9468e5220c7478a68e53606ca2c0a1242aae63
Cole Sashkin <csashceh1024@gmail.com>
2025-08-29 13:49:13 -0700

    update

--- re_assist.py
+++ re_assist.py
@@ -1,8 +1,38 @@
 import ollama
 import os
+import pydantic
 import sqlite3
 
+class DatabaseManager:
+    def __init__(self, db_path='re_assist.db'):
+        self.conn = sqlite3.connect(db_path, autocommit=True)
+        self.create_tables()
 
+    def create_tables(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS conversations (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_input TEXT,
+                    response TEXT,
+                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+                )
+            ''')
+
+    def log_conversation(self, user_input, response):
+        with self.conn.cursor() as cursor:
+            cursor.execute('''
+                INSERT INTO conversations (user_input, response)
+                VALUES (?, ?)
+            ''', (user_input, response))
+
+    def fetch_conversations(self):
+        with self.conn.cursor() as cursor:
+            res = cursor.execute('SELECT * FROM conversations')
+            return res.fetchall()
+        
+    def close(self):
+        self.conn.close()
 
 class REAssistent:
     def __init__(self):
